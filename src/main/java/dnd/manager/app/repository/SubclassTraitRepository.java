8aab2d9acee7f21832eb61e451c627c48579b5d3
corona314 <imanol.eguidon@gmail.com>
2026-03-24 15:40:48 +0100

    .

--- src/main/java/dnd/manager/app/repository/SubclassTraitRepository.java
+++ src/main/java/dnd/manager/app/repository/SubclassTraitRepository.java
@@ -5,9 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dnd.manager.app.model.SubclassTrait;
+import dnd.manager.app.model.SubclassTraitId;
 
 
-public interface SubclassTraitRepository extends JpaRepository<SubclassTrait, Long> {
+public interface SubclassTraitRepository extends JpaRepository<SubclassTrait, SubclassTraitId> {
 
     List<SubclassTrait> findBySubclassIdAndLevelLessThanEqual(Long subclassId, Integer level);
 
