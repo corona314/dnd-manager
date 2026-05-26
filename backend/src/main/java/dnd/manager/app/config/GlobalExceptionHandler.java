bbe593c333b6d06831dc1a0f09d51921a9b8904c
corona314 <imanol.eguidon@gmail.com>
2026-05-26 20:47:31 +0200

    auth deshabilitado hasta front

--- backend/src/main/java/dnd/manager/app/config/GlobalExceptionHandler.java
+++ backend/src/main/java/dnd/manager/app/config/GlobalExceptionHandler.java
@@ -3,12 +3,19 @@ package dnd.manager.app.config;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
-        return ResponseEntity.status(404).body("\033[31m" + e.getMessage() + "\033[0m");
+        return ResponseEntity.status(404).body(e.getMessage());
     }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<String> handleNoResourceFoundException(NoResourceFoundException e) {
+        return ResponseEntity.status(404).body("Route not found: " + e.getMessage());
+    }
+
 }
--- backend/src/main/java/dnd/manager/app/config/SecurityConfig.java
+++ backend/src/main/java/dnd/manager/app/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package dnd.manager.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
@@ -12,6 +14,17 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder PasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
+
+        return http.build();
     }
 
 }
